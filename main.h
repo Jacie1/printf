e49396e4d28bf3c068b3eeece4dc6552e7f7883f
Jacie1 <irinemwanga5@gmail.com>
2022-10-18 11:05:02 -0700

    printf

--- main.h
+++ main.h
@@ -35,7 +35,7 @@
  * @h_modifier: on if h_modifier is specified
  * @l_modifier: on if l_modifier is specified
  *
- */
+*/
 
 typedef struct parameters
 {
@@ -59,7 +59,7 @@ typedef struct parameters
  *
  * @specifier: format token
  * @f: The function associated
- */
+*/
 
 typedef struct specifier
 {
